ec07dc4be7af18180b15244900d3f47da9d6f5c4
Razvan Ciocan <rzv.c93@outlook.com>
2025-01-27 21:27:32 +0000

    Refactored and extended the canvas rendering logic. Replaced redundant code with reusable functions for setting styles (setStyle) and drawing shapes (drawShape). Added dynamic object and tree creation with movement. Introduced a road object for road properties and improved road rendering. Organized updates and rendering for better structure.

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-//
+// variables
 let canvasUpdateInterval;
+let treeCreationInterval;
+let objectCreationInterval;
 
 // time
 const oneSecond = 1000;
 let startTime;
 
+// array
+let trees = [];
+let objects = [];
+
+// style
+function setStyle(font, fillStyle, strokeStyle, lineWidth) {
+  if (font) {
+    ctx.font = font;
+  }
+  if (fillStyle) {
+    ctx.fillStyle = fillStyle;
+  }
+  if (strokeStyle) {
+    ctx.strokeStyle = strokeStyle;
+  }
+  if (lineWidth) {
+    ctx.lineWidth = lineWidth;
+  }
+}
+
 // TIMER
 function drawTimer() {
-  ctx.font = "30px bold Permanent Marker";
-  ctx.fillStyle = "black";
+  setStyle("30px bold Permanent Marker", "black", null, null);
 
-  const elapsedTime = startTime ? Date.now() - startTime : 0;
-  // If startTime has a value, the variable elapsedTime will be given the value Date.now() - startTime.
-  // If startTime does not have a value, elapsedTime will be given the value 0. it shows 00000 before start
+  // Calculate the elapsed time.
+  let elapsedTime;
+  if (!startTime) {
+    elapsedTime = 0;
+  } else {
+    elapsedTime = Date.now() - startTime;
+  }
 
   // Calculate the minutes, seconds and milliseconds of the total elapsed time.
   let milliseconds = Math.floor((elapsedTime % oneSecond) / 100);
@@ -36,36 +61,166 @@ function drawTimer() {
 }
 drawTimer();
 
-function drawRoad() {
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.strokeStyle = "gray";
-  ctx.lineWidth = 4;
-  ctx.rect(0, canvas.height - 350, canvas.width, 200);
-  ctx.fill();
-  ctx.stroke();
-  ctx.closePath();
+// CREATE TREE
+function createTree() {
+  const tree = {
+    width: 10,
+    height: Math.random() * 15 + 55,
+    x: canvas.width + Math.random() * 100,
+    y: 0,
+  };
+  if (Math.random() > 0.5) {
+    tree.y = canvas.height - 350 - tree.height;
+  } else {
+    tree.y = canvas.height - 150;
+  }
+  trees.push(tree);
+}
+createTree();
 
+// CREATE OBJECT
+function createObject() {
+  const object = {
+    width: 40,
+    height: 120,
+    x: canvas.width + Math.random() * 100,
+    y: 0,
+  };
+  if (Math.random() > 0.5) {
+    object.y = canvas.height - 350;
+  } else {
+    object.y = canvas.height - 150 - object.height;
+  }
+  objects.push(object);
+}
+createObject();
+
+// DRAW SHAPE
+function drawShape(
+  type,
+  x,
+  y,
+  width,
+  height,
+  font,
+  fillStyle,
+  strokeStyle,
+  lineWidth
+) {
+  setStyle(font, fillStyle, strokeStyle, lineWidth);
   ctx.beginPath();
-  ctx.strokeStyle = "white";
-  ctx.lineWidth = 4;
-  ctx.moveTo(0, canvas.height - 350 + 200 / 2);
-  ctx.lineTo(canvas.width, canvas.height - 350 + 200 / 2);
+  if (type == "rect") {
+    ctx.rect(x, y, width, height);
+    ctx.fill();
+  } else if (type == "line") {
+    ctx.moveTo(x, y);
+    ctx.lineTo(width, height);
+  }
   ctx.stroke();
   ctx.closePath();
 }
-drawRoad();
+
+// road details
+const road = {
+  x: 0,
+  y: canvas.height - 350,
+  width: canvas.width * 2,
+  height: 200,
+  speed: 1,
+};
+
+// DRAW ROAD SCENE
+function drawRoadScene() {
+  // draw road
+  //  type, x, y, width, height, font, fillStyle, strokeStyle, lineWidth
+  drawShape(
+    "rect",
+    road.x,
+    road.y,
+    road.width,
+    road.height,
+    null,
+    "black",
+    "gray",
+    4
+  );
+
+  // draw line
+  //  type, x, y, width, height, font, fillStyle, strokeStyle, lineWidth
+  drawShape(
+    "line",
+    road.x,
+    road.y + road.height / 2,
+    road.x + road.width * 2,
+    road.y + road.height / 2,
+    null,
+    null,
+    "white",
+    4
+  );
+
+  // draw trees
+  //  type, x, y, width, height, font, fillStyle, strokeStyle, lineWidth
+  for (let i = 0; i < trees.length; ++i) {
+    if (trees[i].x + trees[i].width < 0) {
+      trees.splice(i, 1);
+      --i;
+    } else {
+      trees[i].x -= 1;
+      drawShape(
+        "rect",
+        trees[i].x,
+        trees[i].y,
+        trees[i].width,
+        trees[i].height,
+        null,
+        "green",
+        "brown",
+        1
+      );
+    }
+  }
+
+  // draw objects
+  //  type, x, y, width, height, font, fillStyle, strokeStyle, lineWidth
+  for (let i = 0; i < objects.length; ++i) {
+    if (objects[i].x + objects[i].width < 0) {
+      objects.splice(i, 1);
+      --i;
+    } else {
+      objects[i].x -= 1;
+      drawShape(
+        "rect",
+        objects[i].x,
+        objects[i].y,
+        objects[i].width,
+        objects[i].height,
+        null,
+        "red",
+        "white",
+        1
+      );
+    }
+  }
+}
+drawRoadScene();
 
 // START GAME
 function startGameBtn() {
   startTime = Date.now();
   document.getElementById("start-game-btn").disabled = true;
   canvasUpdateInterval = setInterval(updateCanvas, 100 / 60);
+  treeCreationInterval = setInterval(createTree, 2000);
+  objectCreationInterval = setInterval(createObject, 1500);
 }
 
 // UPDATE CANVAS
 function updateCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawTimer();
-  drawRoad();
+  road.x -= road.speed;
+  if (road.x <= -canvas.width) {
+    road.x = 0;
+  }
+  drawRoadScene();
 }
